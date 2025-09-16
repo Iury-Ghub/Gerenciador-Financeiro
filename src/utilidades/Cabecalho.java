3af45643076fffcb092f78bb972ca9c6e860852e
Iury-Ghub <joseiury74@gmail.com>
2025-09-16 13:29:05 -0300

    Criação de uma Classe para Cabeçalho

--- src/utilidades/Cabecalho.java
+++ src/utilidades/Cabecalho.java
@@ -3,28 +3,28 @@ package utilidades;
 public class Cabecalho {
     public Cabecalho(String titulo, int largura) {
         for (int i = 0; i<largura;i++){
-            System.out.printf("=");
+            System.out.print("=");
         }
         System.out.println();
 
         for (int i = 0; i<largura-titulo.length();i++) {
             if ((largura - titulo.length()) % 2 == 0) {
                 if (i < (largura - titulo.length()) / 2) {
-                    System.out.printf(" ");
+                    System.out.print(" ");
                 }
             }
         }
         for (int i = 0; i<largura-titulo.length();i++) {
             if ((largura - titulo.length()) % 2 != 0) {
                 if (i > (largura - titulo.length()) / 2) {
-                    System.out.printf(" ");
+                    System.out.print(" ");
                 }
             }
         }
         System.out.println(titulo);
 
         for (int i = 0; i<largura;i++){
-            System.out.printf("=");
+            System.out.print("=");
         }
         System.out.println();
     }
